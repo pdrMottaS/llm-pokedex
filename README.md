b7dd9ce5874eb2e13041c48ae1a3faab8ff2e2ec
pdrMottaS <pedro.motta@databot.digital>
2025-05-09 15:33:00 -0300

    add deploy settings

---
+++ README.md
@@ -0,0 +1,8 @@
+# LLM Pokedex 
+
+Do you know that project for begginers (usually coded in React)?
+This is the exactly same project, but is a LLM program :)
+
+## Why?
+
+I don´t know, maybe I was bored
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from controller import pokemon_router
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(pokemon_router)
 
-uvicorn.run(app, port=5000)
+uvicorn.run(app, port=5000, host="0.0.0.0")
